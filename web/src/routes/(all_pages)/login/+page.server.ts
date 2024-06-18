7130fda477c73004fb4dca5d9404730be7bc949c
Japorized <johnson5756@live.com>
2024-06-18 05:58:49 -0400

    [web] Keep pages static

--- web/src/routes/(all_pages)/login/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { userStore } from "$lib/stores/user";
-import { redirect } from "@sveltejs/kit";
-
-export function load() {
-  userStore.set({ username: "John Smith", accessToken: "foo" });
-  throw redirect(307, "/me");
-}
--- web/src/routes/(all_pages)/logout/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import { userStore } from "$lib/stores/user";
-
-export function load() {
-  userStore.set(null);
-
-  throw redirect(307, "/");
-}
-
