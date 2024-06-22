c8b5b047625a04551c6fa66b2c2f821d22866149
Japorized <johnson5756@live.com>
2024-06-21 23:16:11 -0400

    [web] Mock /me page

---
+++ web/src/lib/components/CheckeredPattern.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+enum ColorChoice {
+  Red = "red",
+  Blue = "blue",
+  Green = "green",
+  Yellow = "yellow",
+  Gold = "gold",
+}
+
+export let color: ColorChoice | string = ColorChoice.Red;
+</script>
+
+<svg
+	version="1.1"
+	viewBox="0 0 188 180"
+	xmlns="http://www.w3.org/2000/svg"
+	preserveAspectRatio="xMinYMin meet"
+  class={`checkered-pattern ${color}`}
+>
+  <pattern id={`${color}-tile`} viewBox="0, 0, 10, 10" width=".25" height=".25" patternTransform="rotate(-30)">
+		<rect width="5" height="5" class="main" />
+		<rect x="5" width="5" height="5" class="sub" />
+		<rect y="5" width="5" height="5" class="sub" />
+		<rect x="5" y="5" width="5" height="5" class="main" />
+	</pattern>
+
+  <radialGradient id={`${color}-grad`} cx="50%" cy="50%" r="0.5">
+		<stop opacity="1" offset="0" class="gradient-start-color" />
+		<stop opacity="0" offset="100%" stop-color="white" class="gradient-stop-color" />
+	</radialGradient>
+	<mask id="mask">
+		<!--<polygon
+			points="
+    0 150, 0 60, 30 60, 30 30, 60 30,
+    60 0, 90 0, 90 30, 120 30, 120 0,
+    150 0, 150 30, 180 30, 180 120,
+    150 120, 150 150, 120 150, 120 180,
+    90 180, 90 150, 60 150, 60 180,
+    30 180, 30 150
+      "
+			fill="url(#tile)"
+			class="masking-polygon"
+    />-->
+    <rect width="100%" height="100%" fill={`url(#${color}-tile)`} />
+	</mask>
+
+  <rect width="100%" height="100%" fill={`url(#${color}-grad)`} mask="url(#mask)" />
+</svg>
+
+<style>
+	svg {
+		opacity: 0.7;
+    width: 120px;
+	}
+
+  .gradient-stop-color {
+    stop-color: white;
+  }
+
+	.red .main {
+		fill: red;
+	}
+
+	.red .sub {
+		fill: rgb(255, 150, 150);
+	}
+
+  .red .gradient-start-color {
+    stop-color: red;
+  }
+
+	.blue .main {
+		fill: blue;
+	}
+
+	.blue .sub {
+		fill: rgb(150, 150, 255);
+	}
+
+  .blue .gradient-start-color {
+    stop-color: blue;
+  }
+
+	.green .main {
+		fill: green;
+	}
+
+	.green .sub {
+		fill: rgb(150, 255, 150);
+	}
+
+  .green .gradient-start-color {
+    stop-color: green;
+  }
+
+	.yellow .main {
+		fill: yellow;
+	}
+
+	.yellow .sub {
+		fill: rgb(150, 255, 255);
+	}
+
+  .yellow .gradient-start-color {
+    stop-color: yellow;
+  }
+
+	.gold .main {
+		fill: gold;
+	}
+
+	.gold .sub {
+		fill: rgb(252, 235, 136);
+	}
+
+  .gold .gradient-start-color {
+    stop-color: gold;
+  }
+
+	.masking-polygon {
+		transform-origin: 50% 50%;
+		transform: scale(0.8) skew(-20deg);
+	}
+</style>
---
+++ web/src/routes/(all_pages)/me/+page.svelte
@@ -0,0 +1,271 @@
+<script lang="ts">
+	import { Temporal } from 'temporal-polyfill';
+
+	import FancyHeader from '$lib/components/FancyHeader.svelte';
+	import { userStore } from '$lib/stores/user';
+	import RoundInitial from '$lib/components/RoundInitial.svelte';
+	import Robot from '$lib/components/icons/Robot.svelte';
+	import CheckeredPattern from '$lib/components/CheckeredPattern.svelte';
+	import Icon from '@iconify/svelte';
+
+	// Mocked bot results
+	type Player = {
+		id: string;
+		username: string;
+		profileImg?: string;
+	};
+	type Bot = {
+		id: string;
+		owner?: Player;
+	};
+	type MatchResult = {
+		id: string;
+		timestamp: Temporal.Instant;
+		bot1: Bot;
+		bot2: Bot;
+		winner: 'bot1' | 'bot2';
+	};
+	$: botResults = [
+		{
+			id: 'match111',
+			timestamp: Temporal.Instant.from('2024-07-19T00:31:19Z'),
+			bot1: {
+				id: 'abc123',
+				owner: {
+					id: 'me1',
+					username: 'John Smith',
+				},
+			},
+			bot2: {
+				id: 'bar312',
+				owner: {
+					id: 'foobar1',
+					username: 'Foo Bar 1',
+				},
+			},
+			winner: 'bot1',
+		},
+		{
+			id: 'match110',
+			timestamp: Temporal.Instant.from('2024-07-19T00:26:07Z'),
+			bot1: {
+				id: 'abc123',
+				owner: {
+					id: 'me1',
+					username: 'John Smith',
+				},
+			},
+			bot2: {
+				id: 'bar314',
+				owner: {
+					id: 'foobar2',
+					username: 'Foo Bar 2',
+				},
+			},
+			winner: 'bot2',
+		},
+		{
+			id: 'match109',
+			timestamp: Temporal.Instant.from('2024-07-17T16:59:07Z'),
+			bot1: {
+				id: 'abc123',
+				owner: {
+					id: 'me1',
+					username: 'John Smith',
+				},
+			},
+			bot2: {
+				id: 'bar311',
+				owner: {
+					id: 'foobar3',
+					username: 'Foo Bar 3',
+				},
+			},
+			winner: 'bot1',
+		},
+	] as MatchResult[];
+
+	const localTimezoneId = Temporal.Now.timeZoneId();
+
+	$: buckets = botResults.reduce(
+		(acc, cur) => {
+			const zonedDt = cur.timestamp.toZonedDateTimeISO(localTimezoneId);
+			const localizedDate = zonedDt.toPlainDate().toLocaleString();
+			if (Object.hasOwn(acc, localizedDate)) {
+				acc[localizedDate] = [...acc[localizedDate], cur];
+			} else {
+				acc[localizedDate] = [cur];
+			}
+
+			return acc;
+		},
+		{} as { [key: string]: MatchResult[] },
+	);
+</script>
+
+<main>
+	<FancyHeader headerLevel="1">
+		<RoundInitial slot="icon" name={$userStore?.username} />
+		{$userStore?.username}
+	</FancyHeader>
+
+	<div class="content-container">
+		<article class="my-match-results">
+			<h2>Bot Results</h2>
+			{#each Object.entries(buckets) as bucket}
+				<section class="match-result-group">
+					<h3>
+						<span>{bucket[0]}</span>
+					</h3>
+					{#each bucket[1] as matchResult}
+						<article class="match-result">
+							<time
+								datetime={matchResult.timestamp
+									.toZonedDateTimeISO(localTimezoneId)
+									.toPlainTime()
+									.toLocaleString()}
+							>
+								{matchResult.timestamp
+									.toZonedDateTimeISO(localTimezoneId)
+									.toPlainTime()
+									.toLocaleString()}
+							</time>
+
+							<div class="match-result-summary">
+								<figure class="bot-wrapper">
+									<CheckeredPattern color={matchResult.winner === 'bot1' ? 'red' : 'blue'} />
+									<Robot hasCrown={matchResult.winner === 'bot1'} />
+									<RoundInitial name={matchResult.bot1.owner?.username} />
+								</figure>
+								vs
+								<figure class="bot-wrapper">
+									<CheckeredPattern color={matchResult.winner === 'bot2' ? 'red' : 'blue'} />
+									<Robot hasCrown={matchResult.winner === 'bot2'} />
+									<RoundInitial name={matchResult.bot2.owner?.username} />
+								</figure>
+							</div>
+							<a href={`/matches/${matchResult.id}`}><Icon icon="tabler:video" /></a>
+						</article>
+					{/each}
+				</section>
+			{/each}
+		</article>
+	</div>
+</main>
+
+<style>
+	main {
+		border: 1px solid gray;
+		padding: 0.3em 1.5em 1em 0em;
+		border-radius: 5px 5px 10px 10px;
+		min-height: 30vh;
+		width: 60vw;
+		margin: 2em auto 5em auto;
+		background: white;
+	}
+
+	main :global(.fancy-header) {
+		max-width: 60%;
+	}
+
+	main :global(.fancy-header .person-initials) {
+		width: 1em;
+	}
+
+	.content-container {
+		padding-top: 1.5em;
+		padding-left: 1.5em;
+	}
+
+	.match-result-group + .match-result-group {
+		margin-top: 2em;
+	}
+
+	.match-result-group > h3 {
+		position: relative;
+		padding-left: 1em;
+		font-size: 1.6rem;
+	}
+
+	.match-result-group > h3::before {
+		content: '';
+		position: absolute;
+		top: 50%;
+		left: 0;
+		width: 100%;
+		height: 3px;
+		background: black;
+	}
+
+	.match-result-group > h3 span {
+		position: relative;
+		padding: 0 1em;
+		background: white;
+		z-index: 1;
+	}
+
+	.match-result {
+		display: grid;
+		grid-template-columns: 1fr 2fr 1fr;
+		column-gap: 1.5em;
+		align-items: center;
+		position: relative;
+	}
+
+	.match-result *:nth-child(3) {
+		justify-self: center;
+		font-size: 3rem;
+	}
+
+	.match-result + .match-result {
+		margin-top: 1rem;
+	}
+	.match-result + .match-result::before {
+		content: '';
+		width: 70%;
+		height: 1px;
+		background: linear-gradient(
+			0.25turn,
+			transparent,
+			15%,
+			rgba(100, 100, 100, 0.5),
+			85%,
+			transparent
+		);
+		position: absolute;
+		top: -0.5rem;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+
+	.match-result-summary {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+	.bot-wrapper {
+		position: relative;
+		width: 120px;
+		height: 120px;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.bot-wrapper :global(.checkered-pattern) {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: inherit;
+	}
+	.bot-wrapper :global(.robot) {
+		position: relative;
+		width: 100px;
+		z-index: 1;
+	}
+	.bot-wrapper :global(.person-initials) {
+		position: absolute;
+		z-index: 2;
+		top: 65%;
+		right: 10px;
+	}
+</style>
